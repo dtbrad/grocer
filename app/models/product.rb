83acfdda80710d19f5e86734910b8f9fb6d2861c
Daniel Brad <danieltbrad@gmail.com>
2016-11-14 20:38:06 -0800

    Add product index and show actions + views
    - Also add method for displaying how many times a product is bought

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :line_items
   has_many :baskets, through: :line_items
+
+  def times_bought
+    line_items.inject(0){|sum,l| sum + l.quantity }
+  end
+
 end
