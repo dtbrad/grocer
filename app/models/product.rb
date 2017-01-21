3fb5af6041d05f35aa288f124b487e52973bf12b
danieltbrad <danieltbrad@gmail.com>
2017-01-21 10:29:19 -0800

    Refactor product.rb for style

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,32 +5,48 @@ class Product < ApplicationRecord
   validates :name, presence: true
 
   def self.custom_sort(category, direction)
-    if category == "times_bought"
-      products = select('products.*', 'SUM(line_items.quantity) as line_items_sum').
-      group('products.id').
-      order("line_items_sum #{direction}")
-    elsif category == "highest_price"
-      products = select('products.*', 'MAX(line_items.price_cents) as max_price').
-      group('products.id').
-      order("max_price #{direction}")
-    elsif category == "lowest_price"
-      products = select('products.*', 'MIN(line_items.price_cents) as min_price').
-      group('products.id').
-      order("min_price #{direction}")
-    elsif category == "most_recently_purchased"
-      products = select('products.*', 'MAX(baskets.date) as max_date').
-      group('products.id').
-      order("max_date #{direction}")
-    elsif category == "nickname"
-      products = select('products.*').
-      group('products.id').
-      order("nickname #{direction}")
+    case category
+    when 'times_bought'
+      sort_most_bought(direction)
+    when 'highest_price'
+      sort_highest_price(direction)
+    when 'lowest_price'
+      sort_lowest_price(direction)
+    when 'most_recently_purchased'
+      sort_recently_purchased(direction)
     else
-      products = select('products.*').
-      group('products.id').
-      order('products.name')
+      sort_nickname(direction)
     end
-    products
+  end
+
+  def self.sort_most_bought(direction)
+    select('products.*', 'SUM(line_items.quantity) as line_items_sum')
+      .group('products.id')
+      .order("line_items_sum #{direction}")
+  end
+
+  def self.sort_highest_price(direction)
+    select('products.*', 'MAX(line_items.price_cents) as max_price')
+      .group('products.id')
+      .order("max_price #{direction}")
+  end
+
+  def self.sort_lowest_price(direction)
+    select('products.*', 'MIN(line_items.price_cents) as min_price')
+      .group('products.id')
+      .order("min_price #{direction}")
+  end
+
+  def self.sort_recently_purchased(direction)
+    select('products.*', 'MAX(baskets.date) as max_date')
+      .group('products.id')
+      .order("max_date #{direction}")
+  end
+
+  def self.sort_nickname(direction)
+    select('products.*')
+      .group('products.id')
+      .order("nickname #{direction}")
   end
 
   def times_bought(user)
@@ -42,7 +58,8 @@ class Product < ApplicationRecord
   end
 
   def highest_price_by_user(user)
-    line_items.where(basket: Basket.where(user: user)).order(:price_cents).last.price
+    line_items.where(basket: Basket.where(user: user))
+              .order(:price_cents).last.price
   end
 
   def lowest_price
@@ -50,7 +67,8 @@ class Product < ApplicationRecord
   end
 
   def lowest_price_by_user(user)
-    line_items.where(basket: Basket.where(user: user)).order(:price_cents).first.price
+    line_items.where(basket: Basket.where(user: user))
+              .order(:price_cents).first.price
   end
 
   def self.most_expensive_product
@@ -58,7 +76,8 @@ class Product < ApplicationRecord
   end
 
   def self.most_expensive_product_by_user(user)
-    LineItem.where(basket: Basket.where(user: user)).order(price_cents: :desc).first.product
+    LineItem.where(basket: Basket.where(user: user))
+            .order(price_cents: :desc).first.product
   end
 
   def self.least_expensive_product
@@ -66,7 +85,8 @@ class Product < ApplicationRecord
   end
 
   def self.least_expensive_product_by_user(user)
-    LineItem.where(basket: Basket.where(user: user)).order(:price_cents).first.product
+    LineItem.where(basket: Basket.where(user: user))
+            .order(:price_cents).first.product
   end
 
   def self.most_popular_product
@@ -74,8 +94,8 @@ class Product < ApplicationRecord
   end
 
   def self.stable_price
-    x = all.select {|p| p.line_items.collect {|l| l.price}.uniq.count > 1}
-    x.each {|p| puts p.name}
+    x = all.select { |p| p.line_items.collect { |l| l.price }.uniq.count > 1 }
+    x.each { |p| puts p.name }
   end
 
   def most_recently_purchased(user)
@@ -86,12 +106,11 @@ class Product < ApplicationRecord
     line_items.where(basket: Basket.where(user: user))
   end
 
-  def self.most_popular_product
-    filtered_products.joins(:line_items).group('products.id').order('SUM(quantity)').last
-  end
-
   def self.filtered_products
-    Product.where.not(name: ['Beer Bottle Dep', 'Beer Deposit 30 C', 'Beer Deposit 60 C', '$5 Off $30 Offer', '$10 Off Coupon', '25% Wine Discount', 'Bag Refund', 'Bag It Forward', '$5 Off $30 offer', '$5 Off Coupon', 'Beer Deposit 30C'])
+    Product.where.not(name: ['Beer Bottle Dep', 'Beer Deposit 30 C',
+                             'Beer Deposit 60 C', '$5 Off $30 Offer',
+                             '$10 Off Coupon', '25% Wine Discount',
+                             'Bag Refund', 'Bag It Forward', '$5 Off $30 offer',
+                             '$5 Off Coupon', 'Beer Deposit 30C'])
   end
-
 end
