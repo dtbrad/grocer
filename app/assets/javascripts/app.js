492395fb551d2d81cb78df6da348f63ffa9a8590
danieltbrad <danieltbrad@gmail.com>
2017-02-22 17:29:55 -0800

    Configure initial angular routes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,19 @@
-angular.module('listmaker', [])
+angular.module('listmaker', ['templates', 'ui.router'])
+.config(function($locationProvider, $httpProvider, $stateProvider, $urlRouterProvider){
+  $locationProvider.hashPrefix('')
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  $stateProvider
+  .state('home', {
+    url: "",
+    templateUrl: "app/templates/home.html"
+  })
+  .state('home.newList', {
+    url: "/newlist",
+    templateUrl: "app/templates/list.html"
+  })
+  .state('home.allLists', {
+    url: "/lists",
+    templateUrl: "app/templates/lists.html"
+  })
+  $urlRouterProvider.otherwise("/lists")
+});
