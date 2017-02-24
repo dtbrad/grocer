4137da2db2efd9146a121c06ed9a2081c20ba3b6
danieltbrad <danieltbrad@gmail.com>
2017-02-24 15:07:36 -0800

    Add list data to emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
   default from: 'from@example.com'
   layout 'mailer'
 end
--- app/mailers/list_mailer.rb
+++ app/mailers/list_mailer.rb
@@ -1,9 +1,10 @@
 class ListMailer < ActionMailer::Base
   default from: "from@example.com"
+  add_template_helper(ApplicationHelper)
 
   def list_mailer(list)
     @list = list
     @user = list.user
-    mail(to: @user.email, subject: 'Sample Email')
+    mail(to: @user.email, subject: @list.name)
   end
 end
