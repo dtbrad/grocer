b10c4c018d8ea26bc76369ed3e1286e9bad425f5
danieltbrad <danieltbrad@gmail.com>
2017-02-24 13:33:09 -0800

    Configure action mailer and premailer gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
