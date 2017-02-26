8739797e7b7db96f826b6184b5b626fc16a19ebf
danieltbrad <danieltbrad@gmail.com>
2017-02-26 10:25:10 -0800

    install devise, create devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223063107) do
+ActiveRecord::Schema.define(version: 20170226182233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,13 +62,24 @@ ActiveRecord::Schema.define(version: 20170223063107) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.string   "email"
+    t.string   "email",                  default: "", null: false
     t.string   "image"
     t.string   "oauth_token"
     t.string   "oauth_refresh_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
