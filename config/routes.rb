6ea096e7b22b77e46ffbae335d5406c64b4641bf
danieltbrad <danieltbrad@gmail.com>
2017-02-26 17:11:47 -0800

    Refactor sign in/up navigation, logic and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'application#welcome'
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
   resources :shopping_lists
   resources :baskets
   resources :products
 
-  root 'baskets#index'
   get 'welcome', to: 'application#welcome'
-
   get 'remove', to: 'baskets#remove', as: 'remove'
 
   get 'monthly_spending_chart', to: 'charts#monthly_spending'
@@ -15,10 +21,8 @@ Rails.application.routes.draw do
   get 'most_bought_chart', to: 'charts#most_bought'
   get 'product_monthly_purchasing_chart', to: 'charts#product_monthly_purchasing'
   get 'product_weekly_purchasing_chart', to: 'charts#product_weekly_purchasing'
-
   get 'product_summaries', to: 'products#product_summaries'
 
   get 'listmaker', to: 'angular#angular'
   get 'listmaker/*all', to: 'angular#angular'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
