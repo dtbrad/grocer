7a8921734b09785251257b0709314df53f151e86
danieltbrad <danieltbrad@gmail.com>
2017-03-01 09:56:18 -0800

    Redirect to sign in page after session logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
       response.headers["Vary"] = "accept"
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
