ec3ba9ec96e6df9b2adf0a74b224e403c7effc82
danieltbrad <danieltbrad@gmail.com>
2017-03-03 12:55:47 -0800

    Enable sidekiq for new scraper method

--- app/workers/basket_worker.rb
+++ app/workers/basket_worker.rb
@@ -2,9 +2,7 @@ class BasketWorker
   include Sidekiq::Worker
 
   def perform(id, date, token)
-    # binding.pry
     user = User.find(id)
     Scraper.process_emails(user, date, token)
-    # Do something
   end
 end
