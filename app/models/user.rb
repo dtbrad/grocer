d96a4e1f6791e977e9a7a6b1f84722e839696421
danieltbrad <danieltbrad@gmail.com>
2017-03-27 09:39:00 -0700

    Allow user to register via omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,14 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
+    unless user
+      user = User.create(
+        name: data["name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20],
+        confirmed_at: Time.now
+      )
+    end
     user
   end
 end
