d738a291bb879d3beebeeb68b99269b159379f13
danieltbrad <danieltbrad@gmail.com>
2017-03-28 20:18:53 -0700

    Add footer & retain line items on basket deletion

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,6 +1,6 @@
 class Basket < ApplicationRecord
   belongs_to :user
-  has_many :line_items, dependent: :destroy
+  has_many :line_items
   has_many :products, through: :line_items
   validates :date, presence: true
   paginates_per 10
