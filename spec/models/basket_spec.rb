aa2ca6f3bd548be1e21ac7040924903f516ccaeb
Daniel Brad <danieltbrad@gmail.com>
2017-08-03 11:45:57 -0700

    Refactor basket_spec

--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -37,44 +37,44 @@ describe Basket do
       @basket5 =  create(:basket, :two_items_two_total, date: Time.parse('2017-03-14 08:59:54 -0800'))
       @basket6 =  create(:basket, :five_items_five_total, date: Time.parse('2017-04-14 08:59:54 -0800'))
       @basket7 =  create(:basket, :two_items_twenty_total, date: Time.parse('2017-05-14 08:59:54 -0800'))
-
-      @graph_config1 = GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-04-14 08:59:54 -0800',
-                                       unit: 'month')
-      @graph_config2 = GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-02-14 08:59:54 -0800',
-                                       unit: 'week')
-      @graph_config3 = GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-01-20 08:59:54 -0800',
-                                       unit: 'day')
-      @baskets1 = Basket.from_graph(@graph_config1)
-      @baskets2 = Basket.from_graph(@graph_config2)
-      @baskets3 = Basket.from_graph(@graph_config3)
     end
 
+    let(:graph_config1) { GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-04-14 08:59:54 -0800',
+                                     unit: 'month') }
+    let(:graph_config2) { GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-02-14 08:59:54 -0800',
+                                     unit: 'week') }
+    let(:graph_config3) { GraphConfig.new(start_date: '2017-01-14 08:59:54 -0800', end_date: '2017-01-20 08:59:54 -0800',
+                                     unit: 'day') }
+
+    let(:baskets1) { Basket.from_graph(graph_config1) }
+    let(:baskets2) { Basket.from_graph(graph_config2) }
+    let(:baskets3) { Basket.from_graph(graph_config3) }
+
     it 'returns an ActiveRecord association relation from the graph_config object' do
-      expect(@baskets1.class).to eq(Basket::ActiveRecord_Relation)
+      expect(baskets1.class).to eq(Basket::ActiveRecord_Relation)
     end
 
     it 'includes baskets from inside the time window set by the graph_config object' do
-      expect(@baskets1.include?(@basket6)).to eq(true)
+      expect(baskets1.include?(@basket6)).to eq(true)
     end
 
     it 'does not include baskets from outside the time window set by the graph_config object' do
-      expect(@baskets1.include?(@basket7)).to eq(false)
+      expect(baskets1.include?(@basket7)).to eq(false)
     end
 
     it 'groups properly' do
-      baskets1 = @baskets1.group_baskets(@graph_config1)
-      baskets2 = @baskets2.group_baskets(@graph_config2)
-      baskets3 = @baskets2.group_baskets(@graph_config3)
-      # binding.pry
+      grouped_baskets1 = baskets1.group_baskets(graph_config1)
+      grouped_baskets2 = baskets2.group_baskets(graph_config2)
+      grouped_baskets3 = baskets2.group_baskets(graph_config3)
 
-      expect(baskets1).to eq([[Date.parse('2017-01-01'), 10000], [Date.parse('2017-02-01'),500],
+      expect(grouped_baskets1).to eq([[Date.parse('2017-01-01'), 10000], [Date.parse('2017-02-01'),500],
                              [Date.parse('2017-03-01'), 200], [Date.parse('2017-04-01'), 500]])
 
-      expect(baskets2).to eq([[Date.parse('2017-01-08'), 3000], [Date.parse('2017-01-15'), 7000],
+      expect(grouped_baskets2).to eq([[Date.parse('2017-01-08'), 3000], [Date.parse('2017-01-15'), 7000],
                              [Date.parse('2017-01-22'), 0], [Date.parse('2017-01-29'), 0],
                              [Date.parse('2017-02-05'), 0], [Date.parse('2017-02-12'), 500]])
 
-      expect(baskets3).to eq([[Date.parse('2017-01-14'), 3000], [Date.parse('2017-01-15'), 0],
+      expect(grouped_baskets3).to eq([[Date.parse('2017-01-14'), 3000], [Date.parse('2017-01-15'), 0],
                              [Date.parse('2017-01-16'), 0], [Date.parse('2017-01-17'), 0],
                              [Date.parse('2017-01-18'), 2000], [Date.parse('2017-01-19'), 5000],
                              [Date.parse('2017-01-20'), 0]])
@@ -82,24 +82,18 @@ describe Basket do
 
     context 'Custom Sorting' do
       it 'can sort by date' do
-        expect(@baskets1.custom_sort('sort_date', 'asc').first).to eq(@basket1)
-        expect(@baskets1.custom_sort('sort_date', 'asc').last).to eq(@basket6)
-        expect(@baskets1.custom_sort('sort_date', 'desc').first).to eq(@basket6)
-        expect(@baskets1.custom_sort('sort_date', 'desc').last).to eq(@basket1)
+        expect(baskets1.custom_sort('sort_date', 'asc').first).to eq(@basket1)
+        expect(baskets1.custom_sort('sort_date', 'desc').first).to eq(@basket6)
       end
 
       it 'can sort by total' do
-        expect(@baskets1.custom_sort('sort_total', 'asc').first).to eq(@basket5)
-        expect(@baskets1.custom_sort('sort_total', 'asc').last).to eq(@basket3)
-        expect(@baskets1.custom_sort('sort_total', 'desc').first).to eq(@basket3)
-        expect(@baskets1.custom_sort('sort_total', 'desc').last).to eq(@basket5)
+        expect(baskets1.custom_sort('sort_total', 'asc').first).to eq(@basket5)
+        expect(baskets1.custom_sort('sort_total', 'desc').first).to eq(@basket3)
       end
 
       it 'can sort by number of items' do
-        expect(@baskets1.custom_sort('sort_items', 'asc').first).to eq(@basket5)
-        expect(@baskets1.custom_sort('sort_items', 'asc').last).to eq(@basket3)
-        expect(@baskets1.custom_sort('sort_items', 'desc').first).to eq(@basket3)
-        expect(@baskets1.custom_sort('sort_items', 'desc').last).to eq(@basket5)
+        expect(baskets1.custom_sort('sort_items', 'asc').first).to eq(@basket5)
+        expect(baskets1.custom_sort('sort_items', 'desc').first).to eq(@basket3)
       end
     end
   end
